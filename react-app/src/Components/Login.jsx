8466302c5ddf5d4286cbf396faf06ff4ae33f9fe
tarjanyi.levi@gmail.com <tarjanyi.levi@gmail.com>
2026-02-05 08:26:04 +0100

    Refactor project structure: update package names, add new dependencies, and enhance styling across components

--- react-app/src/Components/Login.jsx
+++ react-app/src/Components/Login.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import "../App.css";
 
 const Login = () => {
   const login = async () => {
+    
+  }
+  const myStlye = {
     
   }
   
