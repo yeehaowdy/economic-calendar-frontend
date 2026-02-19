a49b7c28e4301af41c74f611aa90d0384ddb62b7
botondterenyi <botiterenyi@gmail.com>
2026-02-19 08:55:12 +0100

    update API endpoint in Calendar component to use environment variable

--- react-app/src/Components/Calendar/Calendar.jsx
+++ react-app/src/Components/Calendar/Calendar.jsx
@@ -36,7 +36,7 @@ const Calendar = () => {
   }, [filterCurrency, filterImpact, timeZone]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/api/calendar")
+    fetch(`${process.env.BACKEND_URL}/api/calendar`)
       .then(res => res.json())
       .then(data => {
         setEvents(Array.isArray(data) ? data : []);
